db0c3ecddf32ea5e3b5c1aad32cb0a522a51a470
Rohit Kumar Maurya <rohitmaurya3545@gmail.com>
2025-08-23 15:58:09 +0530

    Final HashRouter + base path fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { HashRouter } from 'react-router-dom'; // <-- add this
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <HashRouter>          {/* <-- wrap here */}
+      <App />
+    </HashRouter>
   </StrictMode>
 );
